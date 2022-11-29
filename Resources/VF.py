97913bb178f951beeb20ec4fb00765daf9564f0b
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2022-11-29 20:21:54 +0100

    Update VF.py

--- Resources/VF.py
+++ Resources/VF.py
@@ -32,8 +32,8 @@ def Initialien():
 """
     Stdout_Output(Header)
 
-def Open_URL(MAX_COUNT, SLEEP_SECONDS, Counter = 0):
-    with open(argv[1], 'r') as f:
+def Open_URL(TEXT_FILE, MAX_COUNT, SLEEP_SECONDS, Counter = 0):
+    with open(TEXT_FILE, 'r') as f:
         for URL in f.read().splitlines():
             if ('http' not in URL): browser_open(f'http://{URL}')
             else: browser_open(URL)
