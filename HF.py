5ee14e407df09f4b4a34f863509cad5a553057f5
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2022-11-29 19:44:05 +0100

    Create HF.py

---
+++ HF.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Rainer Christian Bjoern Herold
+
+# Author
+__author__ = "Rainer C. B. Herold"
+__copyright__ = "Copyright 2022, Rainer C. B. Herold"
+__credits__ = "Rainer C. B. Herold"
+__license__ = "MIT license"
+__version__ = "0.1"
+__maintainer__ = "Rainer C. B. Herold"
+__status__ = "Production"
+
+# Libraries
+try:
+    from numpy import array
+    from sys import argv
+    from time import sleep
+    import webbrowser
+except ModuleNotFoundError as e: input(f"The module was not found\n\n{e}\n\nPlease confirm with the button 'Return'"), exit()
