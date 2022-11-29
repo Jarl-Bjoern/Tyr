fbd079ba324848e1ce1b6dab8dee539429663d5b
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2022-11-29 20:16:11 +0100

    Update TYR.py

--- TYR.py
+++ TYR.py
@@ -2,50 +2,8 @@
 # -*- coding: utf-8 -*-
 # Rainer Christian Bjoern Herold
 
-# Author
-__author__ = "Rainer C. B. Herold"
-__copyright__ = "Copyright 2022, Rainer C. B. Herold"
-__credits__ = "Rainer C. B. Herold"
-__license__ = "MIT license"
-__version__ = "0.1"
-__maintainer__ = "Rainer C. B. Herold"
-__status__ = "Production"
-
 # Libraries
-try:
-    from argparse import ArgumentParser, FileType, RawTextHelpFormatter, SUPPRESS
-    from numpy import array
-    from sys import argv
-    from time import sleep
-    import webbrowser
-except ModuleNotFoundError as e: input(f"The module was not found\n\n{e}\n\nPlease confirm with the button 'Return'"), exit()
-
-# Functions
-def Stdout_Output(Text_Array):
-    for char in Text_Array:
-        stdout.write(char)
-        stdout.flush()
-        sleep(0.01)
-
-def Initialien():
-    class Colors:
-        GREEN = '\033[32m'
-        ORANGE = '\033[33m'
-        BLUE = '\033[34m'
-        UNDERLINE = '\033[4m'
-        RESET = '\033[0m'
-
-    if (osname == 'nt'): system('cls')
-    else: system('clear')
-    Header = """
-💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀
-💀\t\t\t\t\t\t\t\t💀
-💀\t\t      """+Colors.UNDERLINE+"Tyr"+Colors.RESET+ """\t\t\t💀
-💀\t\t\t  """+Colors.ORANGE+"Version "+Colors.BLUE+"0.1"+Colors.RESET+"""\t\t\t\t💀
-💀\t\t\t\t\t\t\t\t💀
-💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀\n\n
-"""
-    Stdout_Output(Header)
+from Resource.VF import *
 
 # Main
 if __name__ == '__main__':
