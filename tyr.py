e9764c758ef17ad75898d8fe6ea37ae26478db7a
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2022-11-29 19:43:28 +0100

    Create tyr.py

---
+++ tyr.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Rainer Christian Bjoern Herold
+
+# Author
+__author__ = "Rainer C. B. Herold"
+__copyright__ = "Copyright 2022, Rainer C. B. Herold"
+__credits__ = "Rainer C. B. Herold"
+__license__ = "MIT license"
+__version__ = "0.1"
+__maintainer__ = "Rainer C. B. Herold"
+__status__ = "Production"
+
+# Libraries
+try:
+    from numpy import array
+    from sys import argv
+    from time import sleep
+    import webbrowser
+except ModuleNotFoundError as e: input(f"The module was not found\n\n{e}\n\nPlease confirm with the button 'Return'"), exit()
+
+# Functions
+def Stdout_Output(Text_Array):
+    for char in Text_Array:
+        stdout.write(char)
+        stdout.flush()
+        sleep(0.01)
+
+def Initialien():
+    class Colors:
+        GREEN = '\033[32m'
+        ORANGE = '\033[33m'
+        BLUE = '\033[34m'
+        UNDERLINE = '\033[4m'
+        RESET = '\033[0m'
+
+    if (osname == 'nt'): system('cls')
+    else: system('clear')
+    Header = """
+💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀
+💀\t\t\t\t\t\t\t\t💀
+💀\t\t      """+Colors.UNDERLINE+"Tyr"+Colors.RESET+ """\t\t\t💀
+💀\t\t\t  """+Colors.ORANGE+"Version "+Colors.BLUE+"0.1"+Colors.RESET+"""\t\t\t\t💀
+💀\t\t\t\t\t\t\t\t💀
+💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀\n\n
+"""
+    Stdout_Output(Header)
+
+
+
+# Main
+if __name__ == '__main__':
+    Counter = 0
+    with open(argv[1], 'r') as f:
+        for i in f.read().splitlines():
+        		if ('http' not in Text[i]): webbrowser.open(f'http://{Text[i]}')
+        		else: webbrowser.open(Text[i])
+        	Counter += 1
+        	sleep(0.65)
+        	if (Counter == 100):
+        		input('There are 100 tabs opened, please close all and confirm with Return to continue')
+        		Counter = 0
