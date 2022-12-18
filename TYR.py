adca67742438b41b63ef71840a2e70dbeed765f2
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2022-12-18 19:27:25 +0100

    Update TYR.py

--- TYR.py
+++ TYR.py
@@ -8,12 +8,13 @@ from Resources.VF import *
 # Main
 if __name__ == '__main__':
     parser = ArgumentParser(add_help=False, formatter_class=RawTextHelpFormatter)
-    required = parser.add_argument_group('required arguments')
-    required.add_argument('-iL','--import-list', type=str, required=True, help='This parameter specify your targetlist\n\nYour list must look like in this example:\n  - http://192.168.2.1\n  - https://192.168.2.2:8443\n  - http://tomcat-test:8080\n\n-------------------------------------------------------------------------------------')
-    optional = parser.add_argument_group('optional arguments')
-    optional.add_argument('-t','--time', type=float, default=0.65, help='This parameter specify the seconds between the next tab\n\nDefault: 0.65 Seconds\n\n-------------------------------------------------------------------------------------')
-    optional.add_argument('-mt','--max-tabs', type=int, default=100, help='This parameter specify the max open tabs\n\nDefault: 100\n\n-------------------------------------------------------------------------------------')
-    optional.add_argument('-h','--help', action='help', default=SUPPRESS, help='Show this help message and exit.\n\n-------------------------------------------------------------------------------------')
+    required = parser.add_argument_group(Colors.ORANGE+'required arguments'+Colors.RESET)
+    optional = parser.add_argument_group(Colors.ORANGE+'optional arguments'+Colors.RESET)
+    
+    required.add_argument('-iL','--import-list', type=str, required=True, help=Colors.ORANGE+'This parameter specify your targetlist\n\nYour list must look like in this example:'+Colors.RESET+'\n  - http://192.168.2.1\n  - https://192.168.2.2:8443\n  - http://tomcat-test:8080'+Colors.BLUE'\n\n-------------------------------------------------------------------------------------'+Colors.RESET)
+    optional.add_argument('-t','--time', type=float, default=0.65, help=Colors.ORANGE+'This parameter specify the seconds between the next tab\n\n'+Colors.RESET+'Default: 0.65 Seconds'+Colors.BLUE+'\n\n-------------------------------------------------------------------------------------'+Colors.RESET)
+    optional.add_argument('-mt','--max-tabs', type=int, default=100, help=Colors.ORANGE+'This parameter specify the max open tabs\n\n'+Colors.RESET+'Default: 100'+Colors.BLUE+'\n\n-------------------------------------------------------------------------------------'+Colors.RESET)
+    optional.add_argument('-h','--help', action='help', default=SUPPRESS, help=Colors.ORANGE+'Show this help message and exit.'+Colors.BLUE+'\n\n-------------------------------------------------------------------------------------'+Colors.RESET)
     args = parser.parse_args()
 
     Initialien(), Open_URL(args.import_list ,args.max_tabs, args.time)
