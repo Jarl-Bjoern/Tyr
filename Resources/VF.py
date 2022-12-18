26f2eb70db997651cde726f341bfc11a436dc3c4
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2022-12-18 19:30:19 +0100

    Update VF.py

--- Resources/VF.py
+++ Resources/VF.py
@@ -36,11 +36,11 @@ def Stdout_Output(Text_Array):
 def Initialien():
     if (osname == 'nt'): system('cls')
     else: system('clear')
-    Header = """
-💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀
+    Header = """💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀
 💀\t\t\t\t\t\t\t\t💀
-💀\t\t\t      """+Colors.UNDERLINE+"Tyr"+Colors.RESET+ """\t\t\t\t💀
-💀\t\t\t  """+Colors.ORANGE+"Version "+Colors.BLUE+"0.1"+Colors.RESET+"""\t\t\t\t💀
+💀\t\t           """+Colors.UNDERLINE+"Yggdrasil"+Colors.RESET+"""\t\t\t\t💀
+💀\t\t\t  """+Colors.ORANGE+"Version "+Colors.CYAN+"0.8"+Colors.RESET+"""\t\t\t\t💀
+💀\t\tRainer Christian Bjoern Herold\t\t\t💀
 💀\t\t\t\t\t\t\t\t💀
 💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀\n\n
 """
