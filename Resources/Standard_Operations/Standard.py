b6ab793291f79c4aacaccab967a6c5df1203555d
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-02-07 17:54:11 +0100

    Create Standard.py

---
+++ Resources/Standard_Operations/Standard.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Rainer Christian Bjoern Herold
+
+class Standard:
+  def Stdout_Output(Text_Array):
+      for char in Text_Array:
+          stdout.write(char)
+          stdout.flush()
+          sleep(0.01)
+
+  def Initialien():
+      if (osname == 'nt'): system('cls')
+      else: system('clear')
+      Header = """💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀
+  💀\t\t\t\t\t\t\t\t💀
+  💀\t\t              """+Colors.UNDERLINE+"TYR"+Colors.RESET+"""\t\t\t\t💀
+  💀\t\t\t  """+Colors.ORANGE+"Version "+Colors.CYAN+"0.1"+Colors.RESET+"""\t\t\t\t💀
+  💀\t\tRainer Christian Bjoern Herold\t\t\t💀
+  💀\t\t\t\t\t\t\t\t💀
+  💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀\n\n
+  """
+      Stdout_Output(Header)
