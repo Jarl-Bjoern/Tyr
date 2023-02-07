ac5d289753842b03cc4b2b26556a7c93e94b929d
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-02-07 18:34:48 +0100

    Update TYR.py

--- TYR.py
+++ TYR.py
@@ -22,7 +22,7 @@ def main():
     PID = Process_ID("firefox")
     if (PID != None):
         input(Colors.ORANGE+"An already open Firefox instance has been located, it is advised to close it to avoid errors.\n\nThe instance is closed with the 'Return' button.\n\n"+Colors.RESET)
-        kill(PID, SIGKILL)
+        kill(PID, SIGKILL), sleep(1.25)
     Open_URL(args.import_list, args.max_tabs, args.time)
 
 # Main
