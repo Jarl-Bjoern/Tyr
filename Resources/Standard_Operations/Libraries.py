a2202f04d80bf716269a0b4af60c47c82cca319c
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-02-07 18:46:34 +0100

    Update Libraries.py

--- Resources/Standard_Operations/Libraries.py
+++ Resources/Standard_Operations/Libraries.py
@@ -22,3 +22,7 @@ try:
     from time import sleep
     from webbrowser import open as browser_open
 except ModuleNotFoundError as e: input(f"The module was not found\n\n{e}\n\nPlease confirm with the button 'Return'"), exit()
+
+# Argument_Parser
+from Resources.Standard_Operations.ArgParser import Argument_Parser
+args = Argument_Parser()
