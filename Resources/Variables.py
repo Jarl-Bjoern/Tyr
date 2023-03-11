810308d466ec0824236cba7884483c5db0f7f694
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-03-11 16:53:26 +0100

    Update Variables.py

--- Resources/Variables.py
+++ Resources/Variables.py
@@ -14,13 +14,16 @@ def Process_ID(Process_Name):
             return P.pid
 
 def Open_URL(TEXT_FILE, MAX_COUNT, SLEEP_SECONDS, Counter = 0):
-    with open(TEXT_FILE, 'r') as f:
-        for URL in f.read().splitlines():
-            if ('http' not in URL): browser_open(f'http://{URL}', new=0)
-            else: browser_open(URL, new=0)
-            Counter += 1
-            sleep(SLEEP_SECONDS)
-            if (Counter == MAX_COUNT):
-                input(Colors.ORANGE+f'\nThe maximum number '+Colors.RED+'{MAX_COUNT}'+Colors.ORANGE+' of open tabs has been reached\n\nYou can continue with the '+Colors.CYAN+'"Return"'+Colors.ORANGE+' key\n\nWith the key combination '+Colors.CYAN+'"CTRL" + "F4"'+Colors.ORANGE+' you can close open tabs faster'+Colors.RESET)
-                Counter = 0
-        print(Colors.ORANGE+"All available URLs were opened! :)"+Colors.RESET)
+    try:
+        with open(TEXT_FILE, 'r') as f:
+            for URL in f.read().splitlines():
+                if ('http' not in URL): browser_open(f'http://{URL}', new=0)
+                else: browser_open(URL, new=0)
+                Counter += 1
+                sleep(SLEEP_SECONDS)
+                if (Counter == MAX_COUNT):
+                    input(Colors.ORANGE+f'\nThe maximum number '+Colors.RED+'{MAX_COUNT}'+Colors.ORANGE+' of open tabs has been reached\n\nYou can continue with the '+Colors.CYAN+'"Return"'+Colors.ORANGE+' key\n\nWith the key combination '+Colors.CYAN+'"CTRL" + "F4"'+Colors.ORANGE+' you can close open tabs faster'+Colors.RESET)
+                    Counter = 0
+            print(Colors.ORANGE+"All available URLs were opened! :)"+Colors.RESET)
+    except FileNotFoundError:
+        print(Colors.ORANGE+"The specified target file could not be found!"+Colors.RESET)
