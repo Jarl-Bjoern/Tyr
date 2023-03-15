379b1abd165838adae26b7c4de5909b114221b37
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-03-15 20:21:17 +0100

    Update Standard.py

--- Resources/Standard_Operations/Standard.py
+++ Resources/Standard_Operations/Standard.py
@@ -7,21 +7,55 @@ from Resources.Standard_Operations.Colors import Colors
 from Resources.Standard_Operations.Libraries import osname, sleep, stdout, system
 
 class Standard:
-  def Stdout_Output(Text_Array):
-      for char in Text_Array:
-          stdout.write(char)
-          stdout.flush()
-          sleep(0.01)
+    def Stdout_Output(Text_Array):
+        for char in Text_Array:
+            stdout.write(char)
+            stdout.flush()
+            sleep(0.01)
 
-  def Initialien():
-      if (osname == 'nt'): system('cls')
-      else: system('clear')
-      Header = """💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀
-💀\t\t\t\t\t\t\t\t💀
-💀\t\t              """+Colors.UNDERLINE+"TYR"+Colors.RESET+"""\t\t\t\t💀
-💀\t\t\t  """+Colors.ORANGE+"Version "+Colors.CYAN+"0.2"+Colors.RESET+"""\t\t\t\t💀
-💀\t\tRainer Christian Bjoern Herold\t\t\t💀
-💀\t\t\t\t\t\t\t\t💀
-💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀\n\n
-"""
-      Standard.Stdout_Output(Header)
+    def Initialien():
+        if (osname == 'nt'): system('cls')
+        else: system('clear')
+        Header = """💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀
+  💀\t\t\t\t\t\t\t\t💀
+  💀\t\t              """+Colors.UNDERLINE+"TYR"+Colors.RESET+"""\t\t\t\t💀
+  💀\t\t\t  """+Colors.ORANGE+"Version "+Colors.CYAN+"0.2"+Colors.RESET+"""\t\t\t\t💀
+  💀\t\tRainer Christian Bjoern Herold\t\t\t💀
+  💀\t\t\t\t\t\t\t\t💀
+  💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀\n\n
+  """
+        Standard.Stdout_Output(Header)
+
+    def Read_Targets_XML(file_path, Array_Out = [], Array_SSL_Out = []):
+        Protocol, Address, Port, Skip_Attributes = "","","",False
+        for event, elem in ET.iterparse(file_path, events=("end",)):
+            if (event == "end"):
+                if (elem.tag == 'address'):
+                    if (Skip_Attributes != True):
+                        Address = elem.attrib['addr']
+                elif (elem.tag == 'state'):
+                    if (elem.attrib['state'] != "open"):
+                        Skip_Attributes = True
+                elif (elem.tag == 'service'):
+                    if (Skip_Attributes != True):
+                        Protocol = elem.attrib['name']
+                elif (elem.tag == 'port'):
+                    if (Skip_Attributes != True):
+                        Port = elem.attrib['portid']
+                    if (Protocol != "" and Address != "" and Port != ""):
+                        Full_Target = f'{Protocol}://{Address}:{Port}'
+                        Protocol, Address, Port = "","",""
+
+                        if ('ssl://' in Full_Target):
+                            if (Full_Target not in Array_SSL_Out):
+                                Array_SSL_Out.append(Full_Target)
+                        elif ('https://' in Full_Target):
+                            if (Full_Target not in Array_SSL_Out):
+                                Array_SSL_Out.append(Full_Target)
+                                Array_Out.append(Full_Target)
+                        else:
+                            if (Full_Target not in Array_Out):
+                                Array_Out.append(Full_Target)
+                        Full_Target = ""
+
+                    Skip_Attributes = False
