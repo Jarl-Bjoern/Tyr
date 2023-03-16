f9952612979662d4e76bce11a391a2c8e54fb991
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-03-16 18:56:54 +0100

    Update ArgParser.py

--- Resources/Standard_Operations/ArgParser.py
+++ Resources/Standard_Operations/ArgParser.py
@@ -21,13 +21,16 @@ def Argument_Parser(Template_Location = dirname(realpath(__file__)).replace('Res
 """
 
     parser = ArgumentParser(add_help=False, formatter_class=RawTextHelpFormatter, description=Colors.ORANGE+Program_Description+Colors.RESET, usage=SUPPRESS)
-    required = parser.add_argument_group(Colors.ORANGE+'required arguments'+Colors.RESET)
     optional = parser.add_argument_group(Colors.ORANGE+'optional arguments'+Colors.RESET)
-    
-    required.add_argument('-iL','--import-list', type=str, required=True, help=Colors.GREEN+'This parameter specify your targetlist\n\nYour list must look like in this example:'+Colors.RESET+'\n  - http://192.168.2.1\n  - https://192.168.2.2:8443\n  - http://tomcat-test:8080\n  - http://tomcat-test:8080/manager'+Colors.BLUE+'\n\n-------------------------------------------------------------------------------------'+Colors.RESET)
+    target   = parser.add_argument_group(Colors.ORANGE+'target arguments'+Colors.RESET)
+
     optional.add_argument('-t','--time', type=float, default=0.65, help=Colors.GREEN+'This parameter specify the seconds between the next tab\n\n'+Colors.RESET+'Default: 0.65 Seconds'+Colors.BLUE+'\n\n-------------------------------------------------------------------------------------'+Colors.RESET)
     optional.add_argument('-mt','--max-tabs', type=int, default=100, help=Colors.GREEN+'This parameter specify the max open tabs\n\n'+Colors.RESET+'Default: 100'+Colors.BLUE+'\n\n-------------------------------------------------------------------------------------'+Colors.RESET)
     optional.add_argument('-h','--help', action='help', default=SUPPRESS, help=Colors.GREEN+'Show this help message and exit.'+Colors.BLUE+'\n\n-------------------------------------------------------------------------------------'+Colors.RESET)
+
+    target_arguments.add_argument('-iL', '--import-list', type=str, help=Colors.GREEN+'Import your target list in the following example:\n  - http://192.168.2.2\n  - https://192.168.2.3\n  - https://192.168.2.4:8443\n  - ssh://192.168.2.5:22\n  - ssl://192.168.2.5:3389'+Colors.BLUE+'\n\n-------------------------------------------------------------------------------------'+Colors.RESET)    
+    target_arguments.add_argument('-t', '--target', type=str, nargs='*', help=Colors.GREEN+'Specify a single or multiple targets like in the following example:\n   - 127.0.0.1, http://127.0.0.1, https://127.0.0.1'+Colors.BLUE+'\n\n-------------------------------------------------------------------------------------'+Colors.RESET)
+    target_arguments.add_argument('-aNx', '--add-nmap-xml-result', type=str, help=Colors.GREEN+'Import your nmap-xml-results as your targets.'+Colors.BLUE+'\n\n-------------------------------------------------------------------------------------'+Colors.RESET)
 
     del ArgumentParser, RawTextHelpFormatter, SUPPRESS
     return parser.parse_args()
