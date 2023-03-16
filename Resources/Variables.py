2d43beb92bf6111e4d639968130e5de012198e6a
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-03-16 18:58:41 +0100

    Update Variables.py

--- Resources/Variables.py
+++ Resources/Variables.py
@@ -13,17 +13,9 @@ def Process_ID(Process_Name):
         if (Process_Name in P.name()):
             return P.pid
 
-def Read_File(Input_File):
-    try:
-        with open(Input_File, 'r') as f:
-            return f.read().splitlines()
-    except FileNotFoundError:
-        print(Colors.RED+"\tThe specified target file could not be found!"+Colors.RESET)
-
-def Open_URL(Input_File, MAX_COUNT, SLEEP_SECONDS, Counter = 0):
+def Open_URL(Input_File, args, MAX_COUNT, SLEEP_SECONDS, Counter = 0):
     for URL in Read_File(Input_File):
-        if ('http' not in URL): browser_open(f'http://{URL}', new=0)
-        else: browser_open(URL, new=0)
+        browser_open(URL, new=0)
         Counter += 1
         sleep(SLEEP_SECONDS)
         if (Counter == MAX_COUNT):
