73412c3ea08b15ae40458c65f594b52182f4524c
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-03-16 18:50:33 +0100

    Update Variables.py

--- Resources/Variables.py
+++ Resources/Variables.py
@@ -13,17 +13,20 @@ def Process_ID(Process_Name):
         if (Process_Name in P.name()):
             return P.pid
 
-def Open_URL(TEXT_FILE, MAX_COUNT, SLEEP_SECONDS, Counter = 0):
+def Read_File(Input_File):
     try:
-        with open(TEXT_FILE, 'r') as f:
-            for URL in f.read().splitlines():
-                if ('http' not in URL): browser_open(f'http://{URL}', new=0)
-                else: browser_open(URL, new=0)
-                Counter += 1
-                sleep(SLEEP_SECONDS)
-                if (Counter == MAX_COUNT):
-                    input(Colors.ORANGE+'\n\tThe maximum number '+Colors.RED+f'{MAX_COUNT}'+Colors.ORANGE+' of open tabs has been reached!\n\n\t   You can continue with the '+Colors.CYAN+'"Return"'+Colors.ORANGE+' key.\n\nWith the key combination '+Colors.CYAN+'"CTRL" + "F4"'+Colors.ORANGE+' you can close open tabs faster'+Colors.RESET)
-                    Counter = 0
-            print(Colors.ORANGE+"All available URLs were opened! :)"+Colors.RESET)
+        with open(Input_File, 'r') as f:
+            return f.read().splitlines()
     except FileNotFoundError:
         print(Colors.RED+"\tThe specified target file could not be found!"+Colors.RESET)
+
+def Open_URL(Input_File, MAX_COUNT, SLEEP_SECONDS, Counter = 0):
+    for URL in Read_File(Input_File):
+        if ('http' not in URL): browser_open(f'http://{URL}', new=0)
+        else: browser_open(URL, new=0)
+        Counter += 1
+        sleep(SLEEP_SECONDS)
+        if (Counter == MAX_COUNT):
+            input(Colors.ORANGE+'\n\tThe maximum number '+Colors.RED+f'{MAX_COUNT}'+Colors.ORANGE+' of open tabs has been reached!\n\n\t   You can continue with the '+Colors.CYAN+'"Return"'+Colors.ORANGE+' key.\n\nWith the key combination '+Colors.CYAN+'"CTRL" + "F4"'+Colors.ORANGE+' you can close open tabs faster'+Colors.RESET)
+            Counter = 0
+    print(Colors.ORANGE+"All available URLs were opened! :)"+Colors.RESET)       
