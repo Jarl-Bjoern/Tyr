831dd67cab38d04fdcf3cf5fc4840af52368705e
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-04-04 18:48:38 +0200

    Update Variables.py

--- Resources/Header_Files/Variables.py
+++ Resources/Header_Files/Variables.py
@@ -7,6 +7,9 @@ from .Libraries import *
 from ..Standard_Operations.Colors import Colors
 from ..Standard_Operations.Standard import Standard
 
+# Dictionaries
+Dict_State = {'State': []}
+
 # Functions
 def Process_ID(Process_Name):
     for P in process_iter():
@@ -16,9 +19,11 @@ def Process_ID(Process_Name):
 def Open_URL(Array_Targets, MAX_COUNT, SLEEP_SECONDS, Counter = 0):
     for URL in Array_Targets:
         browser_open(URL, new=0)
+        if (URL not in Dict_State['State']):
+            Dict_State['State'].append(URL)
         Counter += 1
         sleep(SLEEP_SECONDS)
         if (Counter == MAX_COUNT):
             input(Colors.ORANGE+'\n\tThe maximum number '+Colors.RED+f'{MAX_COUNT}'+Colors.ORANGE+' of open tabs has been reached!\n\n\t   You can continue with the '+Colors.CYAN+'"Return"'+Colors.ORANGE+' key.\n\nWith the key combination '+Colors.CYAN+'"CTRL" + "F4"'+Colors.ORANGE+' you can close open tabs faster'+Colors.RESET)
             Counter = 0
-    print(Colors.ORANGE+"All available URLs were opened! :)"+Colors.RESET)       
+    print(Colors.ORANGE+"All available URLs were opened! :)"+Colors.RESET)
