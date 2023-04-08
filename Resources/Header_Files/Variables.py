420afef4b825723883046dc03908e28ac3c5939e
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-04-08 20:49:54 +0200

    Update Variables.py

--- Resources/Header_Files/Variables.py
+++ Resources/Header_Files/Variables.py
@@ -26,9 +26,17 @@ def Open_URL(Array_Targets, MAX_COUNT, SLEEP_SECONDS, Counter = 0, Current_Step 
         if (URL not in Dict_State['State']):
             Dict_State['State'].append(URL)
         Counter += 1
+        Current_Step += 1
         sleep(SLEEP_SECONDS)
         if (Counter == MAX_COUNT):
-            input(Colors.ORANGE+'\n\tThe maximum number '+Colors.RED+f'{MAX_COUNT}'+Colors.ORANGE+' of open tabs has been reached!\n\n\t   You can continue with the '+Colors.CYAN+'Return'+Colors.ORANGE+' key.\n\nWith the key combination '+Colors.CYAN+'CTRL + F4'+Colors.ORANGE+' you can close open tabs faster'+Colors.RESET)
+            try:    Current_State = Current_Step/Max_Length)*100
+            except: Current_State = Max_Length
+            input(
+                Colors.ORANGE+'\n\tThe maximum number '+Colors.RED+f'{MAX_COUNT}'+Colors.ORANGE+' of open tabs has been reached!\n\n\t   You can continue with the '
+                +Colors.CYAN+'Return'+Colors.ORANGE+' key.\n\nWith the key combination '+Colors.CYAN+'CTRL + F4'
+                +Colors.ORANGE+' you can close open tabs faster\n\n'+Colors.RESET
+                +Colors.RED+f'{round(Current_State, 2)} %'+Colors.ORANGE' of the targets were opened.'+Colors.RESET
+            )
             Counter = 0
             Standard.Print_Header()
     print(Colors.ORANGE+"All available URLs were opened! :)"+Colors.RESET)
