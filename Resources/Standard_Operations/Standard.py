0641aa4304daf0e1090f42e871c42f0e214548c0
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-10-01 17:39:12 +0200

    Update Standard.py

--- Resources/Standard_Operations/Standard.py
+++ Resources/Standard_Operations/Standard.py
@@ -50,7 +50,8 @@ class Standard:
                 if (event == "end"):
                     if (elem.tag == 'address'):
                         if (Skip_Attributes != True):
-                            Address = elem.attrib['addr']
+                            if (elem.attrib['addrtype'] == 'ipv4'):
+                                Address = elem.attrib['addr']
                     elif (elem.tag == 'state'):
                         if (elem.attrib['state'] != "open"):
                             Skip_Attributes = True
